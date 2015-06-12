06e82c4960681a6666f51c3f6976f3048949ff69
Samuel Goodwin <samuel@roundwallsoftware.com>
2015-06-12 16:50:13 -0700

    Initial Commit

---
+++ LoginExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LoginExample
+//
+//  Created by Samuel Goodwin on 6/12/15.
+//  Copyright (c) 2015 Roundwall Software. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ LoginExampleTests/LoginExampleTests.swift
@@ -0,0 +1,36 @@
+//
+//  LoginExampleTests.swift
+//  LoginExampleTests
+//
+//  Created by Samuel Goodwin on 6/12/15.
+//  Copyright (c) 2015 Roundwall Software. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class LoginExampleTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
