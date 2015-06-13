943e0813a9a86421a2b27d4c9bfef1bbd35959de
Samuel Goodwin <samuel@roundwallsoftware.com>
2015-06-13 12:47:51 +0200

    All set.

--- LoginExample/ViewController.swift
+++ LoginExample/ViewController.swift
@@ -8,18 +8,65 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+enum LoginStatus {
+    case LoggedOut
+    case LoggedIn(apiKey: String)
+}
 
+extension UIViewController {
+    func embedFullScreenController(controller: UIViewController){
+        controller.willMoveToParentViewController(self)
+        controller.view.frame = view.bounds
+        view.addSubview(controller.view)
+        addChildViewController(controller)
+    }
+}
+
+class LoggedInViewController: UIViewController {
+    var status: LoginStatus?
+}
+
+class LoggedOutViewController: UIViewController {
+    var gatewayViewController: GatewayViewController?
+    
+    @IBAction func login() {
+        // Assume login magically worked just fine.
+        gatewayViewController?.status = .LoggedIn(apiKey: "someimportantapikey")
+    }
+}
+
+class GatewayViewController: UIViewController {
+    var status = LoginStatus.LoggedOut {
+        didSet {
+            showAppropriateController()
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        showAppropriateController()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    private func removeOldChildren() {
+        for controller in childViewControllers as! [UIViewController] {
+            controller.removeFromParentViewController()
+        }
     }
-
-
+    
+    private func showAppropriateController() {
+        removeOldChildren()
+        
+        switch status {
+        case .LoggedOut:
+            let controller = storyboard?.instantiateViewControllerWithIdentifier("loggedOut") as? LoggedOutViewController
+            controller?.gatewayViewController = self
+            map(controller, embedFullScreenController)
+        case .LoggedIn(let apiKey):
+            let controller = storyboard?.instantiateViewControllerWithIdentifier("loggedIn") as? LoggedInViewController
+            controller?.status = self.status
+            map(controller, embedFullScreenController)
+        }
+    }
 }
 
